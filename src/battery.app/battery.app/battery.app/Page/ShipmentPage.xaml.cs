2e37be87cac9a399c9f0bb2ab6f72e6a5cbff7b9
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-27 18:00:37 +0300

    Страница продаж

--- src/battery.app/battery.app/battery.app/Page/ShipmentPage.xaml.cs
+++ src/battery.app/battery.app/battery.app/Page/ShipmentPage.xaml.cs
@@ -16,5 +16,10 @@ namespace battery.app.Page
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new SalePage(), false);
+        }
     }
 }
