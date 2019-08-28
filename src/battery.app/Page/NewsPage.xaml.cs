a77f4bffa4f7cbd5fa77265500ada40a31b7a957
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-28 16:35:34 +0300

    Верстка детальной новости + замена на PRO иконки

--- src/battery.app/Page/NewsPage.xaml.cs
+++ src/battery.app/Page/NewsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using battery.app.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace battery.app.Page
         public NewsPage()
         {
             InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new NewsDetail(), false);
         }
     }
 }
