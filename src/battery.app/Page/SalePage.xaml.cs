93f3d1112a1d469005412bc3acda4446812bb3a3
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-29 18:01:17 +0300

    Шрифты для страниц

--- src/battery.app/Page/SalePage.xaml.cs
+++ src/battery.app/Page/SalePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using battery.app.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace battery.app.Page
         private void Retry_goods(object sender, EventArgs e)
         {
             Navigation.PopModalAsync(false);
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new DetailGoods(), false);
         }
     }
 }
