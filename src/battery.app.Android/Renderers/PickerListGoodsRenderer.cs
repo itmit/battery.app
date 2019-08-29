31d86f14f28a08608f844ab57c2bc804c5072749
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-29 15:59:21 +0300

    Кастомный Picker

---
+++ src/battery.app.Android/Renderers/PickerListGoodsRenderer.cs
@@ -0,0 +1,58 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Support.V4.Content;
+using battery.app.Controls;
+using battery.app.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(PickerListGoods), typeof(PickerListGoodsRenderer))]
+namespace battery.app.Droid.Renderers
+{
+    public class PickerListGoodsRenderer : PickerRenderer
+    {
+        public PickerListGoodsRenderer(Context context) : base(context)
+        {
+        }
+
+        PickerListGoods element;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            element = (PickerListGoods)this.Element;
+
+            if (Control != null && this.Element != null && !string.IsNullOrEmpty(element.Image))
+                Control.Background = AddPickerStyles(element.Image);
+
+        }
+
+        public LayerDrawable AddPickerStyles(string imagePath)
+        {
+            ShapeDrawable border = new ShapeDrawable();
+            border.Paint.Color = Android.Graphics.Color.Transparent;
+           // border.SetPadding(10, 10, 10, 10);
+            border.Paint.SetStyle(Paint.Style.Stroke);
+
+            Drawable[] layers = { border, GetDrawable(imagePath) };
+            LayerDrawable layerDrawable = new LayerDrawable(layers);
+            layerDrawable.SetLayerInset(0, 0, 0, 0, 0);
+
+            return layerDrawable;
+        }
+
+        private BitmapDrawable GetDrawable(string imagePath)
+        {
+            int resID = Resources.GetIdentifier(imagePath, "drawable", this.Context.PackageName);
+            var drawable = ContextCompat.GetDrawable(this.Context, resID);
+            var bitmap = ((BitmapDrawable)drawable).Bitmap;
+
+            var result = new BitmapDrawable(Resources, Bitmap.CreateScaledBitmap(bitmap, 10, 6, true));
+            result.Gravity = Android.Views.GravityFlags.Right;
+
+            return result;
+        }
+    }
+}
---
+++ src/battery.app/Controls/PickerListGoods.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Xamarin.Forms;
+
+namespace battery.app.Controls
+{
+    public class PickerListGoods : Picker
+    {
+        public static readonly BindableProperty ImageProperty =
+            BindableProperty.Create(nameof(Image), typeof(string), typeof(PickerListGoods), string.Empty);
+
+        public string Image
+        {
+            get { return (string)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+    }
+}
